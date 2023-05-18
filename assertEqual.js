caedded5f6a680a4aa0a2df91722910f6f9a8c96
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-05-18 12:19:40 -0700

    assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 " + actual + " !== " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
