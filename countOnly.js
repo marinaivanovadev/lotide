dca00d4dbf9f5e90160e2673c12db86a27b5b9d0
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-05-23 22:01:06 -0700

    countOnly

---
+++ countOnly.js
@@ -0,0 +1,41 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}; //  define a new empty object
+  for (const item of allItems) { //oop over all the items in the array
+    if (itemsToCount[item]) { // is found in the itemsToCount object
+      if (results[item]) {
+        results[item] += 1; //count all item
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+const result1 = countOnly(firstNames, {"Jason": true, "Karims": true, "Fang": true, "Agouhanna": false});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
