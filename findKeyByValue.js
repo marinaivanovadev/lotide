f987e8eb9cb2116322e04763f7555a2ed909a688
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-05-24 23:05:13 -0700

    update

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,9 +7,12 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
-  for (const keys in object) { // iterate through object by key
-    if (object[keys] === value) {
-      return keys;
+  const keys = Object.keys(object); // array of keys from an object
+  //console.log(keys);
+  for (const key of keys) { //irritate through kyes
+    //console.log(key);
+    if (object[key] === value) { // if find
+      return key;
     }
   }
   return undefined;
