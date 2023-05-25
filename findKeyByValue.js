7de20829a50538e2e58c8ea6708228486dafbd6e
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-05-24 23:00:44 -0700

    initial

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const keys in object) { // iterate through object by key
+    if (object[keys] === value) {
+      return keys;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
