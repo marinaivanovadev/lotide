b9615ab4d9658d792bf66bf4f60833b2fc503005
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-06 13:13:13 -0700

    refactor

--- countOnly.js
+++ countOnly.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {}; //  define a new empty object
   for (const item of allItems) { //oop over all the items in the array
@@ -21,21 +15,4 @@ const countOnly = function(allItems, itemsToCount) {
   return results;
 };
 
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-const result1 = countOnly(firstNames, {"Jason": true, "Karims": true, "Fang": true, "Agouhanna": false});
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+module.exports = countOnly;
