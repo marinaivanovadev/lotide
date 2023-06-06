7e1ba5c147389d90c6211241b89eca2b18719f0b
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-06 12:24:39 -0700

    refactor

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
+
 //Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 const eqArrays = (arrayOne, arrayTwo) => {
   if (arrayOne.length !== arrayTwo.length) { /*check the length*/
@@ -18,10 +12,5 @@ const eqArrays = (arrayOne, arrayTwo) => {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
+module.exports = eqArrays;
 
