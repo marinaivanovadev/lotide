ec12949495f6a9ec72d800f3f2ea10126fc2192e
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-07 16:01:35 -0700

    modified

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,20 +1,25 @@
-const assertArraysEqual = require(`../assertArraysEqual`);
+const assert = require(`chai`).assert;
 const takeUntil = require(`../takeUntil`);
 
 const data1 = [1, 2, 5, 7, 2, -1, 3, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-
-console.log(`---`);
-
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ",");
-console.log(results2);
+
 
 const data3 = [10, 20, 30, 40, 50];
 const results3 = takeUntil(data3, x => x > 25);
-console.log(results3);
 
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-assertArraysEqual(results3, [10, 20]);
+describe("#takeUntil", () => {
+  it(`return numbers until < 0`, () => {
+    assert.deepEqual(results1, [1, 2, 5, 7, 2]);
+  });
+
+  it(`return array until ","`, () => {
+    assert.deepEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+  });
+  it(`return array until 25`, () => {
+    assert.deepEqual(results3, [10, 20]);
+  });
+
+});
