bdb24fd0744fdcce439f3ebc62247c55baa89f5b
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-07 14:50:35 -0700

    update MC eqArray

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,18 @@
-const assertEqual = require(`../assertEqual`);
+const assert = require(`chai`).assert;
 const eqArrays = require(`../eqArrays`);
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+describe("#eqArrays", () => {
+  it(`return true - array with numbers equals array`, () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it(`return false - if array no equals array`, () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it(`return true if array with strings equals array with strings`, () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it(`return false if array no equals array`, () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  
+});
