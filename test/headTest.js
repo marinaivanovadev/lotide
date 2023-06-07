9eaaa95579ddf883c40480da6afbfacdbd664c3b
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-06-07 12:58:19 -0700

    refactor with mocha chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require(`../assertEqual`);
+const assert = require(`chai`).assert;
 const head = require(`../head`);
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([7]), 7);
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '7' for ['7']", () => {
+    assert.strictEqual(head(['7']), '7');
+  });
+
+  it("return undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  it("return Hello for arrray [Hello, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
+
